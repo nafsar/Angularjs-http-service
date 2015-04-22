b005d36c278a23995cbb777f3f53979a5eabaac6
Naser Afsar <nafsar@gmail.com>
2015-04-22 16:13:23 -0600

    added transclude

--- products.js
+++ products.js
@@ -2,12 +2,14 @@
 	app.directive("statResponse",function(){
 		return{
 			restrict:"E",
+			transclude: true,
 			templateUrl:"response-status.html"
 		};
 	});
 	app.directive("jsonpService",function(){
 		return{
 			restrict:"E",
+			transclude: true,
 			templateUrl:"jsonp-service.html"
 		};
 	});
